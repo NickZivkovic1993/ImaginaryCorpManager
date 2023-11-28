e015bd347c27f91914a2550b64fd2d424d824ab6
NickZivkovic1993 <NickZivkovic@proton.me>
2023-11-28 14:38:04 +0100

    project creation(web api framework 4.7.2) and identity auth beggining

---
+++ ICMDataManager/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ICMDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
