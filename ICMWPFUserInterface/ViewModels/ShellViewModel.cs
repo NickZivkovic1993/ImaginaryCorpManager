844cc06392ab784716b828eace8f0d45de65f097
NickZivkovic1993 <NickZivkovic@proton.me>
2024-01-29 15:34:39 +0100

    Replaced container with IoC in ShellViewModel

--- ICMWPFUserInterface/ViewModels/ShellViewModel.cs
+++ ICMWPFUserInterface/ViewModels/ShellViewModel.cs
@@ -14,25 +14,28 @@ namespace ICMWPFUserInterface.ViewModels
         
         private IEventAggregator _events;
         private SalesViewModel _salesVM;
-        private SimpleContainer _container;
+        //private SimpleContainer _container;
         public ShellViewModel(IEventAggregator events,
-                              SalesViewModel salesVM,
-                              SimpleContainer container)
+                              SalesViewModel salesVM)
         {
             _events = events;
             //subscribe the current instance of the class
             //So it knows whos asking
             _events.Subscribe(this);
             _salesVM = salesVM;
-            _container = container;
+           // _container = container;
 
 
 
             //keep in mind only one one active item at the time learned that the hard way...
             //check Conductor<> def for more info
-            ActivateItem(_container.GetInstance<LoginViewModel>());
+            //ActivateItem(_container.GetInstance<LoginViewModel>());
             //fixed with transient type instance^^
             //when item is not active loginVM goes away and doesn't have data from previous call
+
+            // Refactor to so i don't need a container from above i'll just coment it as everything else and remove it from constructor
+            // Basically learned Caliburn.Micro could do this
+            ActivateItem(IoC.Get<LoginViewModel>());
         }
 
         public void Handle(LogOnEvent message)
